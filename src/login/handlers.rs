adbd19bdcf8769d7762ff577e16c9ccde0797afc
zlindner <zach.lindner@hotmail.com>
2022-02-10 20:02:15 -0500

    Add password validation for login handler

--- src/login/handlers.rs
+++ src/login/handlers.rs
@@ -1,9 +1,15 @@
 use deadpool_postgres::Pool;
+use pbkdf2::{
+    password_hash::{PasswordHash, PasswordVerifier},
+    Pbkdf2,
+};
 
 use crate::packet::Packet;
 
+#[derive(Debug)]
 enum LoginError {
     AccountNotFound,
+    InvalidPassword,
 }
 
 pub async fn login(mut packet: Packet, pool: &Pool) {
@@ -19,11 +25,18 @@ pub async fn login(mut packet: Packet, pool: &Pool) {
         hwid
     );
 
-    get_account(name, pool).await;
+    // check if the account exists and validate the password
+    if let Err(e) = validate_password(name, password, pool).await {
+        log::error!("An error occurred while logging in: {:?}", e);
+        return;
+    }
+
+    // TODO check if banned
+    // TODO check if already logged in
+    // TODO check for tos
 }
 
-//
-async fn get_account(name: String, pool: &Pool) -> Result<(), LoginError> {
+async fn validate_password(name: String, password: String, pool: &Pool) -> Result<(), LoginError> {
     let client = pool.get().await.unwrap();
     let rows = client
         .query("SELECT password FROM accounts WHERE name = $1", &[&name])
@@ -31,11 +44,23 @@ async fn get_account(name: String, pool: &Pool) -> Result<(), LoginError> {
         .unwrap();
 
     if rows.len() == 0 {
-        log::debug!("Account doesn't exist");
+        log::debug!("Account not found with name: {}", name);
         return Err(LoginError::AccountNotFound);
     }
 
-    // account exists, return account
+    // get and parse the hash stored in db
+    let hash: String = rows[0].get(0);
+    let parsed_hash = PasswordHash::new(&hash).unwrap();
+
+    // check the entered password against the parsed hash
+    let valid = Pbkdf2
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .is_ok();
+
+    if !valid {
+        log::debug!("Invalid password for account with name: {}", name);
+        return Err(LoginError::InvalidPassword);
+    }
 
     Ok(())
 }
