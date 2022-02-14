6bc99820cd788d7205e85d51a13188de9c5af762
zlindner <zach.lindner@hotmail.com>
2022-02-14 16:12:55 -0500

    Add ssl configuration for connecting to heroku postgres db

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ use deadpool_postgres::{Manager, Pool};
 use dotenv::dotenv;
 use log::LevelFilter;
 use login::server::LoginServer;
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
+use postgres_openssl::MakeTlsConnector;
 use simple_logger::SimpleLogger;
 use std::env;
 use std::error::Error;
-use tokio_postgres::NoTls;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -28,12 +29,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
 
     let mut pg_config = tokio_postgres::Config::new();
+    pg_config.host(&env::var("DATABASE_HOST").unwrap());
+    pg_config.dbname(&env::var("DATABASE_NAME").unwrap());
     pg_config.user(&env::var("DATABASE_USER").unwrap());
     pg_config.password(&env::var("DATABASE_PASSWORD").unwrap());
-    pg_config.dbname(&env::var("DATABASE_NAME").unwrap());
-    pg_config.host("localhost");
 
-    let manager = Manager::new(pg_config, NoTls);
+    let mut ssl_builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    ssl_builder.set_verify(SslVerifyMode::NONE);
+    let connector = MakeTlsConnector::new(ssl_builder.build());
+
+    let manager = Manager::new(pg_config, connector);
     let pool = Pool::builder(manager).max_size(10).build().unwrap();
 
     LoginServer::new().start(&pool).await?;
