f5b0f1516090a1ab94f34cd1af1316c18b163195
Zach Lindner <zach.lindner@hotmail.com>
2022-08-07 19:11:27 -0700

    Add boilerplat tcp server stuff + event handler

--- world/src/main.rs
+++ world/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use event_handler::WorldServerEventHandler;
+use log::LevelFilter;
+use oxide_core::{db, net::Server, redis, Result};
+use simple_logger::SimpleLogger;
+use std::env;
+
+mod event_handler;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    SimpleLogger::new()
+        .with_module_level("tokio_util", LevelFilter::Debug)
+        .with_module_level("mio", LevelFilter::Debug)
+        .env()
+        .init()
+        .unwrap();
+
+    let db = db::new(10).await?;
+    let redis = redis::new()?;
+
+    Server::new(
+        env::var("WORLD_SERVER_ADDR").unwrap(),
+        WorldServerEventHandler::new(db, redis),
+    )
+    .start()
+    .await?;
+
+    Ok(())
 }
